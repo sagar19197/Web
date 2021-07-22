1abfd905221462935f64572c30b00785320e5066
Sagar Suman <sagar19197@iiitd.ac.in>
2021-07-22 23:30:30 +0530

    added animal tiles js project

---
+++ JavaScript/Animals-tiles-js.js
@@ -0,0 +1,81 @@
+function show1(){
+	document.getElementById("part1").innerHTML = "LION";
+	document.getElementById("part1").style.background="white";
+	document.getElementById("part1").style.textAlign="center";
+}
+function hide1(){
+	document.getElementById("part1").innerHTML="";
+		
+}
+function show2(){
+	document.getElementById("part2").innerHTML = "ELEPHANT";
+	document.getElementById("part2").style.background="white";
+	document.getElementById("part2").style.textAlign="center";
+}
+function hide2(){
+	document.getElementById("part2").innerHTML="";
+		
+}
+function show3(){
+	document.getElementById("part3").innerHTML = "WOLF";
+	document.getElementById("part3").style.background="white";
+	document.getElementById("part3").style.textAlign="center";
+}
+function hide3(){
+	document.getElementById("part3").innerHTML="";
+		
+}
+function show4(){
+	document.getElementById("part4").innerHTML = "TIGER";
+	document.getElementById("part4").style.background="white";
+	document.getElementById("part4").style.textAlign="center";
+}
+function hide4(){
+	document.getElementById("part4").innerHTML="";
+		
+}
+function show5(){
+	document.getElementById("part5").innerHTML = "GIRAFFE";
+	document.getElementById("part5").style.background="white";
+	document.getElementById("part5").style.textAlign="center";
+}
+function hide5(){
+	document.getElementById("part5").innerHTML="";
+		
+}
+function show6(){
+	document.getElementById("part6").innerHTML = "DOG";
+	document.getElementById("part6").style.background="white";
+	document.getElementById("part6").style.textAlign="center";
+}
+function hide6(){
+	document.getElementById("part6").innerHTML="";
+		
+}
+function show7(){
+	document.getElementById("part7").innerHTML = "ZEBRA";
+	document.getElementById("part7").style.background="white";
+	document.getElementById("part7").style.textAlign="center";
+}
+function hide7(){
+	document.getElementById("part7").innerHTML="";
+		
+}
+function show8(){
+	document.getElementById("part8").innerHTML = "LEOPARD";
+	document.getElementById("part8").style.background="white";
+	document.getElementById("part8").style.textAlign="center";
+}
+function hide8(){
+	document.getElementById("part8").innerHTML="";
+		
+}
+function show9(){
+	document.getElementById("part9").innerHTML = "CAT";
+	document.getElementById("part9").style.background="white";
+	document.getElementById("part9").style.textAlign="center";
+}
+function hide9(){
+	document.getElementById("part9").innerHTML="";
+		
+}
